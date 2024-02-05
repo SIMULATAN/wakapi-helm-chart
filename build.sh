25b8e178aa673ef92e8b4359e18e8d99772486f5
SIMULATAN <SIMULATAN@proton.me>
2024-02-05 10:42:33 +0100

    [1.1.0] support loading db password from k8s secret & overrideNamespace

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-#!bin/sh
+#!/bin/sh
 
 helm package src/*
 
-helm repo index --url https://andreymaznyak.github.io/wakapi-helm-chart .
+helm repo index --url https://simulatan.github.io/wakapi-helm-chart .
